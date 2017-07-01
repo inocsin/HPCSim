8bcfa315e76d785dc3a964b6c3a8a70c186e1898
inocsin <vcheungyi@163.com>
2017-07-01 14:34:26 +0800

    Some modification in SerDes.py

--- SerDes.py
+++ SerDes.py
@@ -4,48 +4,56 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
-channel_loss_total = [24, 21, 50, 18.4]
-channel_loss_tx = [24, 21, 4.5, 7.5, 18.4, 6]
-channel_loss_rx = [24, 21, 23, 14.8, 40, 50, 18.4]
-
-tx_power = [200, 152, 145, 97, 105.1, 120]
-rx_power = [382, 136, 230, 102, 453, 320, 142.2]
+total_tech_scale = []
+total_channel_loss = [24, 21, 50, 18.4]
 total_power = [602, 288, 403, 247.3]
 
-channel_loss_tx_index = np.argsort(channel_loss_tx)
-channel_loss_tx = np.array(channel_loss_tx)[channel_loss_tx_index]
-tx_power = np.array(tx_power)[channel_loss_tx_index]
+tx_tech_scale = []
+tx_channel_loss = [24, 21, 4.5, 7.5, 18.4, 6]
+tx_power = [200, 152, 145, 97, 105.1, 120]
 
-channel_loss_rx_index = np.argsort(channel_loss_rx)
-channel_loss_rx = np.array(channel_loss_rx)[channel_loss_rx_index]
-rx_power = np.array(rx_power)[channel_loss_rx_index]
+rx_tech_scale = []
+rx_channel_loss = [24, 21, 23, 14.8, 40, 50, 18.4]
+rx_power = [382, 136, 230, 102, 453, 320, 142.2]
 
-tx_fit = np.polyfit(channel_loss_tx, tx_power, 1)
+
+tx_channel_loss_index = np.argsort(tx_channel_loss)
+tx_channel_loss = np.array(tx_channel_loss)[tx_channel_loss_index]
+tx_power = np.array(tx_power)[tx_channel_loss_index]
+
+rx_channel_loss_index = np.argsort(rx_channel_loss)
+rx_channel_loss = np.array(rx_channel_loss)[rx_channel_loss_index]
+rx_power = np.array(rx_power)[rx_channel_loss_index]
+
+tx_fit = np.polyfit(tx_channel_loss, tx_power, 1)
 print tx_fit
-# p1 = np.poly1d(tx_fit)
-# print p1
-rx_fit = np.polyfit(channel_loss_rx, rx_power, 1)
+tx = np.poly1d(tx_fit)
+print tx
+rx_fit = np.polyfit(rx_channel_loss, rx_power, 1)
 print rx_fit
+rx = np.poly1d(rx_fit)
+print rx
+
 
 figure = plt.figure(1, figsize=(16, 8))
 axe1 = figure.add_subplot(121)
 axe2 = figure.add_subplot(122)
+# transceiver
 plt.sca(axe1)
-# tx
 x = np.arange(0.0, 60, 0.01)
 y = x * tx_fit[0] + tx_fit[1]
 plt.plot(x, y, linewidth=2)
-plt.scatter(channel_loss_tx, tx_power, marker='*', edgecolors='r')
+plt.scatter(tx_channel_loss, tx_power, marker='*', edgecolors='r')
 plt.xlabel("Channel Loss/dB")
 plt.ylabel("Power Dissipation/mW")
-plt.title("Transceiver")
+plt.title("Transmitter")
 
+# receiver
 plt.sca(axe2)
 x = np.arange(0.0, 60, 0.01)
-
 y = x * rx_fit[0] + rx_fit[1]
 plt.plot(x, y, linewidth=2)
-plt.scatter(channel_loss_rx, rx_power, marker='*', edgecolors='r')
+plt.scatter(rx_channel_loss, rx_power, marker='*', edgecolors='r')
 plt.xlabel("Channel Loss/dB")
 plt.ylabel("Power Dissipation/mW")
 plt.title("Receiver")
