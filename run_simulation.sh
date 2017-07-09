44eaa3de1090864d892de88cb8b0f99d3fe720b5
inocsin <vcheungyi@163.com>
2017-07-09 20:23:50 +0800

    Finish the run_simulation.sh

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
-SIMULATION_HOME =
-SCRIPT_HOME = `pwd`
+SIMULATION_HOME=/home/zhangyi/omnetpp-5.1.1/samples/hpcsimulator 
+SCRIPT_HOME=/home/zhangyi/HPCSim 
 echo "SIMUALTION_HOME is ${SIMULATION_HOME}"
 echo "SCRIPT_HOME is ${SCRIPT_HOME}"
-PassThroughLatency=(100 200)
+PassThroughLatency=(100 500)
+#PassThroughLatency=(100)
 InjectionRate=(0.1 0.3 0.5 0.7 0.9 1.0 1.3)
+#InjectionRate=(0.1)
 for latency in ${PassThroughLatency[@]}
 do
     for injection in ${InjectionRate[@]}
     do
         #echo $latency
         #echo $injection
+		echo "python $SCRIPT_HOME/FatTreeAuto.py --injection_rate=$injection --pass_through_latency=$latency"
         python $SCRIPT_HOME/FatTreeAuto.py --injection_rate=$injection --pass_through_latency=$latency
-        rm $SIMUALTION_HOME/fat_tree.h $SIMUALTION_HOME/topoconfig.h $SIMUALTION_HOME/fat_tree.ned
+        echo "rm $SIMULATION_HOME/fat_tree.h $SIMULATION_HOME/topoconfig.h $SIMULATION_HOME/fat_tree.ned"
+        rm $SIMULATION_HOME/fat_tree.h $SIMULATION_HOME/topoconfig.h $SIMULATION_HOME/fat_tree.ned
+        echo "mv $SCRIPT_HOME/result/fat_tree.h $SCRIPT_HOME/result/topoconfig.h $SCRIPT_HOME/result/fat_tree.ned $SIMULATION_HOME"
         mv $SCRIPT_HOME/result/fat_tree.h $SCRIPT_HOME/result/topoconfig.h $SCRIPT_HOME/result/fat_tree.ned $SIMULATION_HOME
+        echo "cd $SIMULATION_HOME"
         cd $SIMULATION_HOME
-        make MODE=debug CONFIGNAME=gcc-debug all
+        echo "make MODE=release CONFIGNAME=gcc-release all"
+        make MODE=release CONFIGNAME=gcc-release all
+        echo "./hpcsimulator -r 0 -u Cmdenv -c FatTree --debug-on-errors=true omnetpp.ini"
         ./hpcsimulator -r 0 -u Cmdenv -c FatTree --debug-on-errors=true omnetpp.ini
-        mv results/FatTree-#0.sca $SCRIPT_HOME/result/"FatTree_i${injection}_l${latency}.sca"
+        echo "mv results/FatTree-#0.sca $SCRIPT_HOME/result/"FatTree_i${injection}_l${latency}.sca""
+        mv results/FatTree-#0.sca $SCRIPT_HOME/data/"FatTree_i${injection}_l${latency}.sca"
     done
 done
