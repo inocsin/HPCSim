e643931134be90bd1d3bbe388de770eca8549b01
inocsin <vcheungyi@163.com>
2017-07-11 15:38:56 +0800

    Some modification

--- run_simulation.sh
+++ run_simulation.sh
@@ -5,7 +5,7 @@ echo "SIMUALTION_HOME is ${SIMULATION_HOME}"
 echo "SCRIPT_HOME is ${SCRIPT_HOME}"
 PassThroughLatency=(100 500)
 #PassThroughLatency=(100)
-InjectionRate=(0.1 0.3 0.5 0.7 0.9 1.0 1.3)
+InjectionRate=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3)
 #InjectionRate=(0.1)
 for latency in ${PassThroughLatency[@]}
 do
