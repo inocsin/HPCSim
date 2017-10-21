15278ec2890babcee921f7ab74dc5cad98ac65fe
inocsin <vcheungyi@163.com>
2017-10-21 12:38:49 +0800

    Modified AnalysisData.py

--- AnalysisData.py
+++ AnalysisData.py
@@ -57,7 +57,6 @@ def plotResult():
             file_list.remove('backup')  # remove backup file
         break
 
-    file_list.remove('infinite')
 
     # different curve
     for i in range(len(file_list)):
@@ -155,7 +154,7 @@ def plotResult():
     plt.xlim(0.0, 1.05)
     # plt.ylim(0.0, 200)
     for i in range(len(file_list)):
-        plotData = preprocessData(dataSummary[i], 2, False, 'slope')
+        plotData = preprocessData(dataSummary[i], 2, True, 'increase')
         plt.scatter(plotData[:,0], plotData[:,2] * 1.0e9)
         plt.plot(plotData[:,0], plotData[:,2] * 1.0e9, marker[i]+'-', linewidth=1)
     plt.xlabel("Injection Rate")
